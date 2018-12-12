36403889e62d1d9d9f5864f20ff88fd2fc596c4c
Dean McCafferty <dean_mccafferty@hotmail.co.uk>
2018-12-12 10:31:04 +0000

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,8 @@
+"use-strict";
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 8080;
+
+app.listen(port, ()=> console.log('Listening on port: ' + port));
