829acb3aca1cd68ad8e991ee2556d24db2b2c451
Dean McCafferty <dean_mccafferty@hotmail.co.uk>
2019-01-14 10:20:19 +0000

    Removed some unneeded code

--- client/src/components/EmployeesList.js
+++ client/src/components/EmployeesList.js
@@ -10,7 +10,6 @@ import Paper from "@material-ui/core/Paper";
 
 import IconButton from "@material-ui/core/IconButton";
 import DeleteIcon from "@material-ui/icons/Delete";
-import AddIcon from "@material-ui/icons/Add";
 
 import { connect } from "react-redux";
 import { fetchEmployees, deleteEmployee } from "../actions/employeeActions";
@@ -18,28 +17,9 @@ import { fetchEmployees, deleteEmployee } from "../actions/employeeActions";
 import PropTypes from "prop-types";
 
 class EmployeesList extends Component {
-    // state = {
-    //     employees: [
-    //         { id: uuid(), firstName: "Bob", lastName: "Dylan", email: "bob.dylan@company.com" },
-    //         { id: uuid(), firstName: "Dave", lastName: "Batista", email: "dave.batista@company.com" },
-    //         { id: uuid(), firstName: "John", lastName: "Doe", email: "john.doe@company.com" },
-    //     ],
-    // };
-
     componentDidMount() {
         this.props.fetchEmployees();
     }
-
-    // AddEmployee() {
-    //     const firstName = prompt("Enter First Name");
-    //     const lastName = prompt("Enter Last Name");
-    //     if (firstName && lastName) {
-    //         const email = firstName + "." + lastName + "@company.com";
-    //         this.setState(state => ({
-    //             employees: [...state.employees, { id: uuid(), firstName, lastName, email }],
-    //         }));
-    //     }
-    // }
 
     DeleteEmployee(id) {
         this.props.deleteEmployee(id);
@@ -49,16 +29,6 @@ class EmployeesList extends Component {
         const { employees } = this.props.employees;
         return (
             <Paper>
-                <IconButton
-                    size="small"
-                    color="secondary"
-                    aria-label="Add"
-                    onClick={() => {
-                        this.AddEmployee();
-                    }}
-                >
-                    <AddIcon />
-                </IconButton>
                 <Table>
                     <TableHead>
                         <TableRow>
